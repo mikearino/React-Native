5a6dab3d95112ed71fc2568a8ecfa6fc0332a8d6
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-16 15:09:44 -0800

    change components.

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
-import HeaderScreen from "./src/screens/HeaderScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: HeaderScreen
+    Components: ComponentsScreen
   },
   {
     initialRouteName: "Components",
--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,21 +2,23 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  const greeting = <Text>Hello to you!</Text>;
+  const name = "- Jonathon the Zombie";
 
   return (
     <View>
-      <Text style={styles.textStyle}>This is the components screen</Text>
-      {greeting}
+      <Text style={styles.textStyle}>Boot Up React Native</Text>
+      <Text style={styles.subHeaderStyle}>I like turtles {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 45
   },
-  headerFontSize: 45
+  subHeaderStyle: {
+    fontSize: 20
+  }
 });
 
 export default ComponentsScreen;
