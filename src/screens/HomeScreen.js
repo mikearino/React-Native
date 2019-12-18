1baf750352dced60aa1a1b4b054893b0e2c91154
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-17 16:21:19 -0800

    add touchable opacity and plain buttons.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Rate My Cat!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Rate My Cat!</Text>
+      <Button
+        onPress={() => console.log("button pressed")}
+        title="Go To Components Demo"
+      />
+      <TouchableOpacity onPress={() => console.log("List Pressed.")}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
