af473be24134776c23cec9e222c2e6253dc6cf8f
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-17 17:04:57 -0800

    link up stack navigator.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = props => {
   return (
     <View>
       <Text style={styles.text}>Rate My Cat!</Text>
       <Button
-        onPress={() => console.log("button pressed")}
+        onPress={() => props.navigation.navigate("Components")}
         title="Go To Components Demo"
       />
       <TouchableOpacity onPress={() => console.log("List Pressed.")}>
