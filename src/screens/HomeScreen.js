9628e0179ca96cb130a0c0631968eff9da48629e
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-19 11:20:17 -0800

    assemble counter screen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,6 +17,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Image")}
         title="Go To Image Screen"
       />
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go To Counter Screen"
+      />
     </View>
   );
 };
