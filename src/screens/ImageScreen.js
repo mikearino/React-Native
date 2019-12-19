8585c69f90f26f6070a704ae0aec219fdf602dba
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-19 10:17:19 -0800

    parent child props.

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,10 +5,9 @@ import ImageDetail from "../components/ImageDetail";
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail />
-      <ImageDetail />
-      <ImageDetail />
-      <ImageDetail />
+      <ImageDetail title="forest" />
+      <ImageDetail title="Beach" />
+      <ImageDetail title="Mountain" />
     </View>
   );
 };
