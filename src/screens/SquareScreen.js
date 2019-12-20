49153e639d19358395123758d69c052afec7b2d4
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-19 20:58:28 -0800

    create color counters.

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,8 +1,15 @@
 import React from "React";
 import { View, Text, StyleSheet } from "react-native";
+import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
-  return <Text>Square Screen</Text>;
+  return (
+    <View>
+      <ColorCounter color="red" />
+      <ColorCounter color="blue" />
+      <ColorCounter color="green" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
